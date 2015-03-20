12367af942fe7501290705b9c33f9d3e84a25bea
brunosales <bruno.sales@dclick.com.br>
2015-03-20 12:37:24 -0400

    adicionando teste para autenticação de aplicação #25

---
+++ server/src/test/java/io/redspark/security/ApplicationAuthenticationTest.java
@@ -0,0 +1,43 @@
+package io.redspark.security;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import io.redspark.SescApplicationTest;
+import io.redspark.controller.dto.UserDTO;
+import io.redspark.security.sesc.SescApplicationUser;
+
+import java.util.Base64;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import br.org.sesc.commons.security.SescUser;
+
+public class ApplicationAuthenticationTest extends SescApplicationTest {
+
+    @Autowired
+    private SescApplicationUser sescApplicationUser;
+    
+    @Test
+    public void testAuthenticationSucessfully() {
+	assertThat(sescApplicationUser.size(), greaterThan(0));
+	
+	SescUser appUser = sescApplicationUser.getSescUser(0);
+	
+	String authString = appUser.getLogin() + ":" + appUser.getPassword();
+	byte[] authEncBytes = Base64.getEncoder().encode(authString.getBytes());
+	String authStringEnc = new String(authEncBytes);
+	
+	
+	ResponseEntity<UserDTO> response = get("/me")
+		.header("Authorization", "Basic " + authStringEnc)
+		.status(HttpStatus.OK)
+		.getResponse(UserDTO.class);
+	
+	assertThat(response.getBody().getName(), is(appUser.getLogin()));
+	
+    }
+}
