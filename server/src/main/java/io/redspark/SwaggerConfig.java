df928807f162e1a36dd02ee028c702e64ece02d8
Marcos Vinícius Fernandes <mvfernandes88@gmail.com>
2016-11-04 15:32:34 -0200

    ajuste swagger

--- server/src/main/java/io/redspark/SwaggerConfig.java
+++ server/src/main/java/io/redspark/SwaggerConfig.java
@@ -11,42 +11,55 @@ import org.springframework.context.annotation.Profile;
 
 import com.google.common.base.Predicate;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.ApiKeyVehicle;
+import springfox.documentation.swagger.web.SecurityConfiguration;
 
 @Configuration
 @Profile({DEV, PRODUCAO})
 public class SwaggerConfig {
 
-    @Bean
-    public Docket api(){
+	@Bean
+    public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-            .select()
-	            .apis(RequestHandlerSelectors.any())
-	            .paths(paths())    
-	            .build()
-            .pathMapping("/")
-            .apiInfo(apiInfo());
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(paths())
+                .build()
+                .pathMapping("/")
+                .apiInfo(apiInfo());
     }
-    
+
+    @Bean
+    public SecurityConfiguration security() {
+        return new SecurityConfiguration(
+                "ClientID",
+                "ClientSecret",
+                "sescEstatistico",
+                "sescEstatistico",
+                "Basic c2VzYzpzZXNj",
+                ApiKeyVehicle.HEADER,
+                "Authorization",
+                "");
+    }
+
     private ApiInfo apiInfo() {
-        ApiInfo apiInfo = new ApiInfo(
-            "SESC REST API",
-            "Modelo do SESC.",
-            "1.0.0",
-            "redspark.io",
-            "API License",
-            "API License URL",
-            "/"
-        );
-        
-        return apiInfo;
+        return new ApiInfoBuilder()
+                .description("Sesc Modelo")
+                .license("API License")
+                .licenseUrl("API License URL")
+                .termsOfServiceUrl("Terms")
+                .title("Sesc Modelo API Doc")
+                .version("0.0.1")
+                .build();
     }
-    
-	@SuppressWarnings("unchecked")
-	private Predicate<String> paths() {
-		return or(regex("/*.*"));
-	}
+
+    @SuppressWarnings("unchecked")
+    private Predicate<String> paths() {
+        return or(regex("/*.*"));
+    }
 }
