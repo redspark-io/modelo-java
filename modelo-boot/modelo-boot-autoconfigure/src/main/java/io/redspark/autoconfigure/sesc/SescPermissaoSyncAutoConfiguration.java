77c403a6ea53f6ea89276112876ef602e8c1ac9c
guihguigo <guilherme.alves.vargas@outlook.com>
2017-05-05 15:36:56 -0300

    correcoes de config

--- modelo-boot/modelo-boot-autoconfigure/src/main/java/io/redspark/autoconfigure/sesc/SescPermissaoSyncAutoConfiguration.java
+++ modelo-boot/modelo-boot-autoconfigure/src/main/java/io/redspark/autoconfigure/sesc/SescPermissaoSyncAutoConfiguration.java
@@ -9,31 +9,33 @@ import org.sesc.permissao.sync.loader.impl.XmlPermissionLoader;
 import org.sesc.permissao.sync.service.SyncService;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
+import br.org.sesc.permissao.client.PermissaoServiceClient;
 import br.org.sesc.permissao.client.config.SyncConfiguration;
 
 @Configuration
-@ConditionalOnClass(PermissionLoader.class)
+@ConditionalOnClass({PermissionLoader.class, PermissaoServiceClient.class})
+@AutoConfigureAfter(SescPermissaoClientAutoConfiguration.class)
 public class SescPermissaoSyncAutoConfiguration {
 	@Bean
 	@ConditionalOnMissingBean
-	public PermissionLoader annotatedControllerPermissionLoader() {
+	public AnnotatedControllerPermissionLoader annotatedControllerPermissionLoader() {
 		return new AnnotatedControllerPermissionLoader();
 	}
 
 	@Bean
 	@ConditionalOnMissingBean
 	@ConditionalOnProperty("sesc.permissao.file.path")
-	public PermissionLoader xmPermissionLoader(@Value("${sesc.permissao.file.path}") String file) {
+	public XmlPermissionLoader xmPermissionLoader(@Value("${sesc.permissao.file.path}") String file) {
 		XmlPermissionLoader xmlPermissionLoader = new XmlPermissionLoader();
 		Resource classPathResource = new ClassPathResource(file);
 		xmlPermissionLoader.setFile(classPathResource);
@@ -57,7 +59,7 @@ public class SescPermissaoSyncAutoConfiguration {
 	@Bean
 	@ConditionalOnBean(SyncService.class)
 	@ConditionalOnMissingBean
-	public ApplicationListener<?> syncApplicationListener(final SyncService syncService) {
+	public SyncApplicationListener syncApplicationListener(final SyncService syncService) {
 		return new SyncApplicationListener(syncService);
 	}
 }
