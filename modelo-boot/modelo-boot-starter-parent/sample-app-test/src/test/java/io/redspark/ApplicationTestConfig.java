72e122e0f66a864c97d250720fd015f78edf6be7
Gabriel Suaki <gsuaki@hotmail.com>
2017-05-08 18:05:19 -0300

    Auto configuração pro notificações.

--- modelo-boot/modelo-boot-starter-parent/sample-app-test/src/test/java/io/redspark/ApplicationTestConfig.java
+++ modelo-boot/modelo-boot-starter-parent/sample-app-test/src/test/java/io/redspark/ApplicationTestConfig.java
@@ -6,21 +6,29 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import br.org.sesc.notificacao.client.NotificationServiceClient;
 import br.org.sesc.permissao.client.PermissaoServiceClient;
+import io.redspark.mock.NotificationServiceClientMock;
 import io.redspark.mock.PermissaoServiceClientMock;
 import io.redspark.mock.XmlPermissionLoaderMock;
 
 @Configuration
 @EnableAutoConfiguration
 public class ApplicationTestConfig extends ApplicationConfig {
-	@Bean
-	public PermissaoServiceClient permissaoServiceClient() {
-		return new PermissaoServiceClientMock();
-	}
-	
-	@Bean
-	@Qualifier("xmlPermissionLoader")
-	public PermissionLoader xmlPermissionLoader() {
-		return new XmlPermissionLoaderMock();
-	}
+
+  @Bean
+  public PermissaoServiceClient permissaoServiceClient() {
+    return new PermissaoServiceClientMock();
+  }
+
+  @Bean
+  public NotificationServiceClient notificationServiceClient() {
+    return new NotificationServiceClientMock();
+  }
+
+  @Bean
+  @Qualifier("xmlPermissionLoader")
+  public PermissionLoader xmlPermissionLoader() {
+    return new XmlPermissionLoaderMock();
+  }
 }
