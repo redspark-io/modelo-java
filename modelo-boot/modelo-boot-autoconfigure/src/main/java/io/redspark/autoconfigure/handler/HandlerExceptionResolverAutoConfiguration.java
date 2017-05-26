98714ac6cc86a00e48cdb54297165494d525ba68
Gabriel Suaki <gsuaki@hotmail.com>
2017-05-26 15:29:15 -0300

    Ajuste no HandlerExceptionResolverAutoConfiguration

--- modelo-boot/modelo-boot-autoconfigure/src/main/java/io/redspark/autoconfigure/handler/HandlerExceptionResolverAutoConfiguration.java
+++ modelo-boot/modelo-boot-autoconfigure/src/main/java/io/redspark/autoconfigure/handler/HandlerExceptionResolverAutoConfiguration.java
@@ -1,7 +1,14 @@
 package io.redspark.autoconfigure.handler;
 
-import java.io.IOException;
+import static org.springframework.http.HttpStatus.PRECONDITION_FAILED;
 
+import java.io.IOException;
+import java.util.List;
+
+import javax.validation.ConstraintViolationException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +18,7 @@ import org.springframework.validation.BindException;
 import org.springframework.web.filter.HttpPutFormContentFilter;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import io.redspark.commons.exception.WebException;
 import lombok.extern.log4j.Log4j;
@@ -18,44 +26,71 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 @Configuration
 @ConditionalOnProperty(value = "modelo.boot.exception.handler.enabled", havingValue = "true", matchIfMissing = true)
-public class HandlerExceptionResolverAutoConfiguration {
-	
+public class HandlerExceptionResolverAutoConfiguration extends WebMvcConfigurerAdapter {
+
+  @Autowired
+  @Qualifier("modeloBootExceptionResover")
+  private HandlerExceptionResolver handler;
+
+  @Override
+  public void configureHandlerExceptionResolvers(final List<HandlerExceptionResolver> exceptionResolvers) {
+
+    exceptionResolvers.add(handler);
+
+    super.configureHandlerExceptionResolvers(exceptionResolvers);
+  }
+
   @Bean
   @ConditionalOnMissingBean
   public HttpPutFormContentFilter httpPutFormContentFilter() {
     return new HttpPutFormContentFilter();
   }
-	
-	@Bean
-	@ConditionalOnMissingBean
-	public HandlerExceptionResolver handlerExceptionResolver() {
-		return (request, response, handler, ex) -> {
-			
-		  ModelAndView modelAndView = null;
-			
-		  ex.printStackTrace();
 
-			if (ex.getClass().isAssignableFrom(BindException.class)) {
-				BindException bindex = (BindException) ex;
-				final StringBuilder message = new StringBuilder();
-				bindex.getAllErrors().forEach(a -> message.append(a.getDefaultMessage()).append("\n"));
-				try {
-					response.sendError(HttpStatus.BAD_REQUEST.value(), message.toString());
-					modelAndView = new ModelAndView();
-				} catch (IOException e) {
-					log.error(e.getMessage(), e);
-				}
-			} else if (ex instanceof WebException) {
-				WebException webex = (WebException) ex;
-				try {
-					response.sendError(webex.getStatus().value(), ex.getMessage());
-					modelAndView = new ModelAndView();
-				} catch (IOException e) {
-					log.error(e.getMessage(), e);
-				}
-			}
+  @Bean
+  @ConditionalOnMissingBean(name = "modeloBootExceptionResover")
+  public HandlerExceptionResolver modeloBootExceptionResover() {
+    return (request, response, handler, ex) -> {
 
-			return modelAndView;
-		};
-	}
+      ModelAndView modelAndView = null;
+
+      ex.printStackTrace();
+
+      if (ex.getClass().isAssignableFrom(BindException.class)) {
+
+        BindException bindex = (BindException) ex;
+
+        final StringBuilder message = new StringBuilder();
+        bindex.getAllErrors().forEach(a -> message.append(a.getDefaultMessage()).append("\n"));
+
+        try {
+          response.sendError(HttpStatus.BAD_REQUEST.value(), message.toString());
+          modelAndView = new ModelAndView();
+        } catch (IOException e) {
+          log.error(e.getMessage(), e);
+        }
+      } else if (ex instanceof WebException) {
+
+        WebException webex = (WebException) ex;
+
+        try {
+          response.sendError(webex.getStatus().value(), ex.getMessage());
+          modelAndView = new ModelAndView();
+        } catch (IOException e) {
+          log.error(e.getMessage(), e);
+        }
+      } else if (ex instanceof ConstraintViolationException) {
+
+        ConstraintViolationException cex = (ConstraintViolationException) ex;
+
+        try {
+          response.sendError(PRECONDITION_FAILED.value(), cex.getMessage());
+          modelAndView = new ModelAndView();
+        } catch (IOException e) {
+          log.error(e.getMessage(), e);
+        }
+      }
+
+      return modelAndView;
+    };
+  }
 }
